97f556895ff134b71a2a71a30c60f0c5b04292d8
pcomo24 <pcomo24@gmail.com>
2017-04-25 15:07:32 -0500

    Add files via upload
    through step 4

--- hero-rpg1.py
+++ hero-rpg1.py
@@ -25,13 +25,20 @@ class Hero:
             break
         else:
             print("Invalid inpt {}".format(inpt))
-
+    def alive():
+        hero_health > 0
 
 hero_stats = Hero(10, 5)
 
 class Goblin(Hero):
     pass
-
+    def attack(enemy):
+        hero_health -= goblin_power
+        print("The goblin does {} damage to you.".format(goblin_power))
+        if hero_health <= 0:
+            print("You are dead.")
+    def alive():
+        goblin_health > 0
 goblin_stats = Goblin(6,2)
 
 
@@ -41,7 +48,7 @@ goblin_stats = Goblin(6,2)
 #    goblin_health = 6
 #    goblin_power = 2
 
-    while goblin_health > 0 and hero_health > 0:
+#    while goblin_health > 0 and hero_health > 0:
         print("You have {} health and {} power.".format(hero_health, hero_power))
         print("The goblin has {} health and {} power.".format(goblin_health, goblin_power))
         print()
@@ -67,10 +74,7 @@ goblin_stats = Goblin(6,2)
 
         if goblin_health > 0:
             # Goblin attacks hero
-            hero_health -= goblin_power
-            print("The goblin does {} damage to you.".format(goblin_power))
-            if hero_health <= 0:
-                print("You are dead.")
+
 
 if __name__ == "__main__":
   main()
